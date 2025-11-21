94de354440987fac2b80023d6fd39b319e61d412
Lakshya1003 <malviyalakshyaraj@gmail.com>
2025-11-21 11:24:07 +0530

    21nov

--- Class work/bankAccount.py
+++
@@ -1,9 +0,0 @@
-class BankAccount :
-  def __init__(self,balance):
-    self.balance = balance
-
-  def deposit(self,amount):
-    if (amount <= 0) :
-      print("Deposit have to be greater than zero ")
-    else:
-      pass
